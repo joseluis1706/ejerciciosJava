e9a9e9a815b46fa6c8af68c8d06a680522e21383
José Luis Martínez Navarro <jose.martinez1@softwareone.com>
2022-06-09 08:44:14 -0500

    inicio ejercicios Java

---
+++ ciclo_if.java
@@ -0,0 +1,11 @@
+public class ciclo_if {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        if(num1 > num2){
+            System.out.println("El numero uno es mayor");
+        }else{
+            System.out.println("El numero dos es mayor");
+        }
+    }
+}
---
+++ sumarDosNumeros.java
@@ -0,0 +1,8 @@
+public class sumarDosNumeros{
+    public static void main(String[] args){
+        int numeroUno = 10;
+        int numeroDos = 20;
+        int suma = numeroUno + numeroDos;
+        System.out.println(suma);   
+    }
+}
